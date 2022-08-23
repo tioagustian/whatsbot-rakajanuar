5668e56bb1efb79eac373360913d46294dae6c0f
Tio Agustian <tio.agustian94@gmail.com>
2022-08-23 14:12:11 +0700

    add permit

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,26 @@
 const fs = require('fs');
 const path = require("path");
 
-const folder = './files/example/View Site/';
+const folder = './files/permit/';
 const list = fs.readdirSync(folder);
 
 list.forEach(file => {
     let filePath = folder + file;
     let extension = path.extname(filePath);
     let fileName = path.basename(filePath, extension);
-    console.log(filePath);
+    let mimeType = 'application/pdf';
+    let siteId = fileName.split(' ')[0];
+    let siteName = fileName.split(' ')[1];
+    let json = {
+        siteId: siteId,
+        siteName: siteName,
+        file: {}
+    };
+    
+    json.file.path = filePath;
+    json.file.mimeType = mimeType;
+    json.file.name = fileName;
+    json.file.author = 'admin';
+    fs.writeFileSync(`./files/permit/${siteId}.json`, JSON.stringify(json, null, 2));
+    console.log(json);
 });
